8523a5f72f3a61993196316cce3b8668b9507fc0
mike-reilly-bg <michael.reilly@berkshiregrey.com>
2024-03-23 14:22:58 -0400

    cleanup

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import yaml
 
 import discord
 from discord.ext import commands
-import env
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import env
 import scrapper
 
 settings_file = "settings.yaml"
