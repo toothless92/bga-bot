3322fc454c83b211c9855b01059490822f254586
mike-reilly-bg <michael.reilly@berkshiregrey.com>
2024-03-23 14:23:25 -0400

    cleanup

--- scrapper.py
+++ scrapper.py
@@ -2,11 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 import logging
-import time
 
 class BGA_Page:
     def __init__(self, url, logger=logging.getLogger()):
