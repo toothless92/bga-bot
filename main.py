750454db8c672c443862676e86ec915d55fb7806
mike <mreilly92@gmail.com>
2024-09-07 00:39:37 -0400

    mute/unmute commands

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import builtins
 import copy
 import logging
+import time
 import yaml
 
 import discord
@@ -75,6 +76,14 @@ class Bot:
         async def poke(ctx):
             await self.poke(ctx)
 
+        @self.bot.command(help="mute notifications for 1 hour")
+        async def mute(ctx):
+            await self.mute(ctx)
+
+        @self.bot.command(help="unmute notifications")
+        async def unmute(ctx):
+            await self.unmute(ctx)
+
         with open(settings_file) as stream:
             self.settings = yaml.safe_load(stream)
 
@@ -107,12 +116,15 @@ class Bot:
         self.page_reread_attempts = self.settings["page_reread_attempts"]
         self.page_reread_pause = self.settings["page_reread_pause"]
         self.page_reload_attempts = self.settings["page_reload_attempts"]
+
     
     def run(self):
         self.bot.run(token, reconnect=True)
+
     
     async def on_ready(self):
         self.logger.info(f'We have logged in as {self.client.user}.')
+
 
     async def poke(self, ctx):
         guild_dict = self.get_guild_dict(ctx.guild.name)
@@ -129,7 +141,34 @@ class Bot:
                     await channel.send(info_str)
                 else:
                     self.logger.warning(f'Poke command failed in guild {ctx.guild.name}.')
+    async def mute(self,ctx):
+        guild_dict = self.get_guild_dict(ctx.guild.name)
+        self.logger.info(f"Mute command received for channel {ctx.guild.name}")
+        self.update_mute_start_time(guild_dict)
+        await ctx.send("Notifications muted for one hour.")
 
+
+    async def unmute(self,ctx):
+        guild_dict = self.get_guild_dict(ctx.guild.name)
+        self.logger.info(f"Unmute command received for channel {ctx.guild.name}")
+        self.update_mute_start_time(guild_dict, 0)
+        await ctx.send("Notifications unmuted.")
+
+
+    def update_mute_start_time(self, guild_dict, start_time:int=None):
+        if time is not None:
+            guild_dict["mute_time_start"] = start_time
+        else:
+            guild_dict["mute_time_start"] = int(time.time())
+
+            
+    def get_mute_start_time(self, guild_dict):
+        if "mute_time_start" in list(guild_dict.keys()):
+            return guild_dict["mute_time_start"]
+        else:
+            return None
+
+    
     async def follow(self, ctx, url, friendly_name):
         guild_dict = self.get_guild_dict(ctx.guild.name)
         game_id = guild_dict["next_game_id"]
@@ -150,6 +189,7 @@ class Bot:
         self.write_data_to_file()
         asyncio.create_task(self.monitor_game(game_id, ctx.guild.name))
 
+
     def get_guild_dict(self, guild_name):
         if guild_name not in list(self.all_games_dict.keys()):
             self.all_games_dict[guild_name] = {}
@@ -157,6 +197,7 @@ class Bot:
             self.all_games_dict[guild_name]["next_game_id"] = 0
             self.all_games_dict[guild_name]["players"] = {}
         return self.all_games_dict[guild_name]
+
 
     async def monitor_game(self, game_id, guild_name):
         game_dict = self.all_games_dict[guild_name]["games"][game_id]
@@ -166,70 +207,73 @@ class Bot:
         page_reload_counter = 0
         try:
             while game_id in list(guild_dict["games"].keys()):
-                self.logger.info(f"{'$'*50}\nTasks:\n{asyncio.all_tasks()}")
-                try:
-                    page_reload_counter += 1
-                    page_listener = scrapper.BGA_Page(url, self.logger)
+                if not self.get_mute_start_time(guild_dict):
+                    self.update_mute_start_time(guild_dict, 0)
+                # self.logger.info(f"{'$'*50}\nTasks:\n{asyncio.all_tasks()}")
+                if int(time.time()) - guild_dict["mute_time_start"] > 60*60:
                     try:
-                        get_page_return = page_listener.get_page()
-                        #page loaded
-                        for i in range(1, self.page_reread_attempts):
-                            #try reading page
-                            await asyncio.sleep(self.page_reread_pause)
-                            player_up = page_listener.check_whos_up()
-                            if player_up is None or player_up == 1:
-                                #player up not found
-                                self.logger.info(f"Player up not found for game id {game_id}. Waiting {self.page_reread_pause} seconds and checking page again (attempt {i}/{self.page_reread_attempts}).")
-                                continue
+                        page_reload_counter += 1
+                        page_listener = scrapper.BGA_Page(url, self.logger)
+                        try:
+                            get_page_return = page_listener.get_page()
+                            #page loaded
+                            for i in range(1, self.page_reread_attempts):
+                                #try reading page
+                                await asyncio.sleep(self.page_reread_pause)
+                                player_up = page_listener.check_whos_up()
+                                if player_up is None or player_up == 1:
+                                    #player up not found
+                                    self.logger.info(f"Player up not found for game id {game_id}. Waiting {self.page_reread_pause} seconds and checking page again (attempt {i}/{self.page_reread_attempts}).")
+                                    continue
+                                else:
+                                    #player up found, move on
+                                    break
+                        except:
+                            #error
+                            info_str = f"Loading page for game {game_id} failed: {str(get_page_return)}."
+                            self.logger.info(info_str)
+                            player_up = None
+                                
+                        if player_up is None or player_up == 1:
+                            #player up not found after multiple reread attempts
+                            if page_reload_counter < self.page_reload_attempts:
+                                #reload attempts remaining
+                                info_str = f"Player up not found for game id {game_id}. Ignoring for now. Reload counter {page_reload_counter}/{self.page_reload_attempts}."
+                                self.logger.info(info_str)
                             else:
-                                #player up found, move on
-                                break
-                    except:
-                        #error
-                        info_str = f"Loading page for game {game_id} failed: {str(get_page_return)}."
-                        self.logger.info(info_str)
-                        player_up = None
-                            
-                    if player_up is None or player_up == 1:
-                        #player up not found after multiple reread attempts
-                        if page_reload_counter < self.page_reload_attempts:
-                            #reload attempts remaining
-                            info_str = f"Player up not found for game id {game_id}. Ignoring for now. Reload counter {page_reload_counter}/{self.page_reload_attempts}."
-                            self.logger.info(info_str)
-                        else:
-                            #no reload attempts remaining
-                            info_str = f"[Game {game_id} \"{game_dict["friendly_name"]}]({game_dict["url"]})\" appears to be over. Removing it from the game list."
-                            self.logger.info(info_str)
-                            channel = self.bot.get_channel(game_dict["channel_id"])
-                            if channel is not None and isinstance(channel, discord.TextChannel):
-                                await channel.send(info_str)
-                            self.delete_game(guild_name=guild_name, game_id=game_id)
-                    else:
-                        if player_up != game_dict["last_player_up"] and player_up.strip() != "":
-                            self.write_data_to_file()
-                            info_str = f"{player_up} is up in game {game_id}: [{game_dict["friendly_name"]}]({game_dict["url"]})."
-                            if player_up in list(guild_dict["players"].keys()):
-                                info_str = info_str + f" <@{guild_dict["players"][player_up]["id"]}>"
-                            self.logger.info(info_str)
-                            channel = self.bot.get_channel(game_dict["channel_id"])
-                            if channel is not None and isinstance(channel, discord.TextChannel):
-                                try:
+                                #no reload attempts remaining
+                                info_str = f"[Game {game_id} \"{game_dict["friendly_name"]}]({game_dict["url"]})\" appears to be over. Removing it from the game list."
+                                self.logger.info(info_str)
+                                channel = self.bot.get_channel(game_dict["channel_id"])
+                                if channel is not None and isinstance(channel, discord.TextChannel):
                                     await channel.send(info_str)
-                                    game_dict["last_player_up"] = player_up
-                                finally:
-                                    page_listener.close()
-                                    self.write_data_to_file()
-                                    self.logger.info(f"Finished new player up sequence {player_up}/{game_id}/{guild_name}.")
+                                self.delete_game(guild_name=guild_name, game_id=game_id)
                         else:
-                            self.logger.info(f"Player up found for game {game_id} in {guild_name} ({player_up}). No change. Took {page_reload_counter} page load attempts.")
-                        page_reload_counter=0
-                except Exception as e:
-                    self.logger(f"Exception encountered while monitoring game {game_id} in {guild_name}: {e}")
-                finally:
-                    try:
-                        page_listener.close()
-                    except:
-                        pass
+                            if player_up != game_dict["last_player_up"] and player_up.strip() != "":
+                                self.write_data_to_file()
+                                info_str = f"{player_up} is up in game {game_id}: [{game_dict["friendly_name"]}]({game_dict["url"]})."
+                                if player_up in list(guild_dict["players"].keys()):
+                                    info_str = info_str + f" <@{guild_dict["players"][player_up]["id"]}>"
+                                self.logger.info(info_str)
+                                channel = self.bot.get_channel(game_dict["channel_id"])
+                                if channel is not None and isinstance(channel, discord.TextChannel):
+                                    try:
+                                        await channel.send(info_str)
+                                        game_dict["last_player_up"] = player_up
+                                    finally:
+                                        page_listener.close()
+                                        self.write_data_to_file()
+                                        self.logger.info(f"Finished new player up sequence {player_up}/{game_id}/{guild_name}.")
+                            else:
+                                self.logger.info(f"Player up found for game {game_id} in {guild_name} ({player_up}). No change. Took {page_reload_counter} page load attempts.")
+                            page_reload_counter=0
+                    except Exception as e:
+                        self.logger(f"Exception encountered while monitoring game {game_id} in {guild_name}: {e}")
+                    finally:
+                        try:
+                            page_listener.close()
+                        except:
+                            pass
                 await asyncio.sleep(self.refresh_time)
         finally:
             self.write_data_to_file()
